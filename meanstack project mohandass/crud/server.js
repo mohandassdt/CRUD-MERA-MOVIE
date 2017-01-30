8b5e72c940bb7d673edc6c63f8947693e7fb5beb
mohandassdt <vijimohandass.s@gmail.com>
2017-01-30 01:32:51 -0800

    Add files via upload

---
+++ meanstack project mohandass/crud/server.js
@@ -0,0 +1,38 @@
+
+var logger = require('morgan');
+var express = require('express');
+var routes = require('./routes/movie-crud');
+
+var bodyParser=require('body-parser');
+
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.use('/mymovie', routes)
+
+
+// Only load this middleware in dev mode (important).
+if (app.get('env') === 'development') {
+  var webpackMiddleware = require("webpack-dev-middleware");
+  var webpack = require('webpack');
+
+  var config = require('./webpack.config');
+
+  app.use(webpackMiddleware(webpack(config), {
+    publicPath: "/build",
+
+    headers: { "X-Custom-Webpack-Header": "yes" },
+
+    stats: {
+      colors: true
+    }
+  }));
+
+}
+
+
+
+var server = app.listen(8000, function () {
+  console.log('listening on port 8000');
+});
