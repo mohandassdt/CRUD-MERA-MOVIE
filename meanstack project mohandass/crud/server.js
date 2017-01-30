97edd1f9aae563633511fa82a530d462f963edfe
mohandassdt <vijimohandass.s@gmail.com>
2017-01-30 19:11:37 +0530

    this is a second commit
    cant make a crud between form.html and index.html

--- meanstack project mohandass/crud/server.js
+++ meanstack project mohandass/crud/server.js
@@ -6,13 +6,43 @@ var routes = require('./routes/movie-crud');
 var bodyParser=require('body-parser');
 
 var app = express();
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var dbHost = 'mongodb://localhost:27017/test';
+mongoose.connect(dbHost);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log("Connected to DB");
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 app.use('/mymovie', routes)
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});
 
-// Only load this middleware in dev mode (important).
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {}
+  }));
+});
+
 if (app.get('env') === 'development') {
   var webpackMiddleware = require("webpack-dev-middleware");
   var webpack = require('webpack');
@@ -30,6 +60,19 @@ if (app.get('env') === 'development') {
   }));
 
 }
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {}
+  }));
+});
 
 
 
